6198ac3448f46561d2cf2c0a53ec7f7d605f2e1b
Tegan Sims <tegansims@gmail.com>
2020-01-03 16:12:49 +0000

    + piano keyboard, works clicking & pressing keys

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # laptop-piano
-# laptop-piano
--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 function playSound(event) {
-    const audio = document.querySelector(`audio[data-key="${event.keyCode}"`)
+    // console.log(event.keyCode)
+    // console.log(event.target.dataset.key)
+
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"`) || document.querySelector(`audio[data-key="${event.target.dataset.key}"]`)
     const key = document.querySelector(`.key[data-key="${event.keyCode}"`)
-    
+
     if (!audio) return;
     audio.currentTime = 0;
     audio.play()
-    key.classList.add('playing')
+    // key.classList.add('playing')
+
 }
 
-function removeTransition(event) {
-    if(event.propertyName !== 'transform') return
-    this.classList.remove('playing')
-}
+// function removeTransition(event) {
+//     if(event.propertyName !== 'transform') return
+//     this.classList.remove('playing')
+// }
+
 
 const keys = document.querySelectorAll('.key')
-keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+// keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+
+
+const white = document.querySelectorAll('.white')
+const black = document.querySelectorAll('.black')
+
+white.forEach(w => w.addEventListener('click', playSound) )
+black.forEach(b => b.addEventListener('click', playSound) )
 
 window.addEventListener('keydown', playSound)
